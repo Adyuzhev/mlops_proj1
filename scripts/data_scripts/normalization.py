7f7fdf94b74fa5a4fd5bd9b0b5775b1999c47038
lesha-ml <lesha-31@mail.ru>
2023-10-16 22:10:05 +0500

    first test

--- scripts/data_scripts/normalization.py
+++ scripts/data_scripts/normalization.py
@@ -17,23 +17,26 @@ os.makedirs(os.path.join("data", "stage2"), exist_ok=True)
 
 df_train = pd.read_csv(train_input)
 X_train = df_train.drop(columns='target')
-y_train = df_train['target']
+y_train = df_train['target'].reset_index(drop=True)
 
 df_test = pd.read_csv(test_input)
 X_test = df_test.drop(columns='target')
-y_test = df_test['target']
+y_test = df_test['target'].reset_index(drop=True)
 
 scaler = params["chosen_scaler"]
 
 if scaler == "MinMaxScaler":
     mm_scaler = MinMaxScaler()
-    X_train_norm = mm_scaler.fit_transform(X_train)
-    X_test_norm = mm_scaler.transform(X_test)
+    X_train_scaled = mm_scaler.fit_transform(X_train)
+    X_test_scaled = mm_scaler.transform(X_test)
 
 elif scaler == "StandardScaler":
     st_scaler = StandardScaler()
-    X_train_stand = st_scaler.fit_transform(X_train)
-    X_test_stand = st_scaler.transform(X_test)
+    X_train_scaled = st_scaler.fit_transform(X_train)
+    X_test_scaled = st_scaler.transform(X_test)
 
-pd.concat([X_train, y_train], axis=1).to_csv(train_output, index=None)
-pd.concat([X_test, y_test], axis=1).to_csv(test_output, index=None)
+X_train_scaled = pd.DataFrame(X_train_scaled)
+X_test_scaled = pd.DataFrame(X_test_scaled)
+
+pd.concat([X_train_scaled, y_train], axis=1).to_csv(train_output, index=None)
+pd.concat([X_test_scaled, y_test], axis=1).to_csv(test_output, index=None)
